4eadc86ad011cc5c46c6deb02fd2283287c3f797
root <root@vmi1512737.contaboserver.net>
2024-02-16 13:07:45 +0100

    master

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 import express from "express";
 import session from "express-session";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const app = express();
-app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+  cookie: { 
+    // expires : getTokenExparationTime(),
+    secure: false, // It means that the cookie will only be sent over HTTPS
+    httpOnly: false, // inaccessible to JavaScript on the client side.
+    sameSite:"strict" // inaccessible to JavaScript on the client side.
+  },
+  secret: "asdasdas654d5sd13asdasdas1d32as1d31a3",// This is the secret used to sign the session cookie.
+  resave: false, // when false will only be saved back to the session store if changes were made during the request 
+  saveUninitialized: true, // Forces a session that is "uninitialized" to be saved to the store
+  // name: 'connect.sid', // Customize the cookie name
+}));
 
+app.use(cors({
+  origin: [
+    'https://www.holtrinity.com'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  credentials:true,
+  optionsSuccessStatus: 200,          
+}));
 
-app.get("/",async (req, res) => {
-    res.send("hello world");
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+
+app.get('/', async (req, res) => {
+  res.send('Hello,Test!');
 });
 
-app.get("/home",async (req, res) => {
-    res.send("hello world");
+app.get('/getToken', async (req, res) => {
+  res.cookie('_csrf', "newCsrfToken", {
+    secure: true, // It means that the cookie will only be sent over HTTPS
+    httpOnly: false, // inaccessible to JavaScript on the client side.
+    sameSite: 'none', // send cookie when open  browser's in address bar.
+  });
+
+  // Send the response to the client
+  res.send({ name: 'Hello, TypeScript Express App!' });
 });
 
-app.listen(5000, () => {
-    console.log(`PORT work -> 5000`);
+
+app.listen(process.env.FLOWERS_BACKEND_PORT || 8000, () => {
+  console.log(`PORT work -> ${process.env.FLOWERS_BACKEND_PORT}`);
 });
